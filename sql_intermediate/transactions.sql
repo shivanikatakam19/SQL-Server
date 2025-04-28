83b0697b9a76bebd9c6eee201a2c301d440d52a1
Shivani Katakam <106737124+shivanikatakam19@users.noreply.github.com>
2025-04-28 12:45:06 +0530

    added transactions

---
+++ sql_intermediate/transactions.sql
@@ -0,0 +1,32 @@
+use organizationDB;
+
+-- transaction is a single unit of statements. either whole statements pass or fail
+-- if any one of the statements fail whole transaction will be failed.
+DELIMITER //
+
+create procedure transaction_salary(in cr_emp_id int, in db_emp_id int, in amount int)
+begin
+    declare rows1 int default 0;
+    declare rows2 int default 0;
+
+    start transaction;
+        update employees set salary = salary - amount where employee_id = cr_emp_id;
+        set rows1 = row_count();
+        update employees set salary = salary + amount where employee_id = db_emp_id;
+        set rows2 = row_count();
+        
+        if rows1=0 or rows2=0 then
+		    rollback;
+		    select 'Transaction rolled back beacuse employee_id not found' AS status;
+		else 
+            commit;
+            select 'Transaction completed!' as status;
+        end if;
+       
+END //
+
+DELIMITER ;
+
+call transaction_salary(3,4,1000);
+
+select * from employees;
