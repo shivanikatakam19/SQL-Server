f88ce6b911aced2d56061e3074b13d7d715d768e
Shivani Katakam <106737124+shivanikatakam19@users.noreply.github.com>
2025-04-28 14:43:45 +0530

    added savepoint for transaction

--- sql_intermediate/transactions.sql
+++ sql_intermediate/transactions.sql
@@ -10,9 +10,9 @@ begin
     declare rows2 int default 0;
 
     start transaction;
-        update employees set salary = salary - amount where employee_id = cr_emp_id;
+        update employees set salary = salary - amount where employee_id = db_emp_id;
         set rows1 = row_count();
-        update employees set salary = salary + amount where employee_id = db_emp_id;
+        update employees set salary = salary + amount where employee_id = cr_emp_id;
         set rows2 = row_count();
         
         if rows1=0 or rows2=0 then
@@ -29,4 +29,22 @@ DELIMITER ;
 
 call transaction_salary(3,4,1000);
 
+DELIMITER //
+-- transaction using savepoint
+-- if you commit the transaction the savepoints will be automatically deleted
+create procedure transaction_savepoint(in cr_emp_id int, in db_emp_id int, in amount int)
+begin 
+    
+    start transaction;
+        update employees set salary = salary - amount where employee_id = db_emp_id;
+        savepoint savepoint_debit;
+        update employees set salary = salary + amount where employee_id = cr_emp_id;
+        rollback to savepoint savepoint_debit;
+        commit;
+end // 
+
+DELIMITER ;
+ 
+-- to call the procedure
+call transaction_savepoint(3, 4, 1000);
 select * from employees;
